4a390abb5ca1b199e1c913365e8ee0e98ce1db2a
HSAllenbili <allenishandsome@foxmail.com>
2023-12-03 12:01:08 +0800

    update

--- src/components/root.vue
+++ src/components/root.vue
@@ -5,7 +5,7 @@
       正在同步学生信息
     </template>
   </n-spin>
-  <tools />
+  <tools v-if="toolshow"/>
 </template>
 
 <script lang="ts" setup>
@@ -15,33 +15,33 @@ import { getStudentInfo } from './tools.ts'
 import global from './global.ts'
 const message = useMessage();
 const spinshow = ref(true);
+const toolshow = ref(false);
 const stuinfo = ref();
 
 const getInfo = async () => {
   spinshow.value = true;
-  if (localStorage.getItem("info") != null) {
-    global.Info = JSON.parse(localStorage.getItem("info") as string);
-    stuinfo.value.id = global.Info.data.studentNo;
-    stuinfo.value.name = global.Info.data.studentName;
-  }
-  else {
-    const asyncinfo: any = (await getStudentInfo()).data;
-    if (asyncinfo.code) {
-      message.error("同步学生信息失败，原因：" + asyncinfo.msg);
-      spinshow.value = false;
-    } else {
-      spinshow.value = false;
-      message.success("同步学生信息成功");
-      localStorage.setItem("info", JSON.stringify(asyncinfo));
-      global.Info = JSON.parse(localStorage.getItem("info") as string);
-      stuinfo.value.id = global.Info.data.studentNo;
-      stuinfo.value.name = global.Info.data.studentName;
-    }
+  const asyncinfo: any = (await getStudentInfo()).data;
+  if (asyncinfo.code) {
+    message.error("同步学生信息失败，原因：" + asyncinfo.msg);
+    spinshow.value = false;
+  } else {
+    message.success("同步学生信息成功");
+    localStorage.setItem("info", JSON.stringify(asyncinfo));
+    showinfo();
   }
 }
 
+const showinfo =() =>{
+    global.Info = JSON.parse(localStorage.getItem("info") as string);
+    stuinfo.value.id = global.Info.data.studentNo;
+    stuinfo.value.name = global.Info.data.studentName;
+    spinshow.value = false;
+    toolshow.value = true;
+}
+
 onMounted(() => {
-  getInfo();
+  if (localStorage.getItem("info") != null) showinfo();
+  else getInfo();
 })
 
 </script>
